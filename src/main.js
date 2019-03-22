de0588891653f682e7ad12c75f15aa18c038f2a9
Jarrod Lugo <jarrodlugo@gmail.com>
2019-03-22 17:31:44 -0400

    Use currentTarget instead of target, so a click on text works

--- src/main.js
+++ src/main.js
@@ -119,6 +119,7 @@ export default class StepZilla extends Component {
       // a child step wants to invoke a jump between steps. in this case 'evt' is the numeric step number and not the JS event
       this.setNavState(evt);
     } else { // the main navigation step ui is invoking a jump between steps
+      const targetValue = evt.currentTarget.value;
       // if stepsNavigation is turned off or user clicked on existing step again (on step 2 and clicked on 2 again) then ignore
       if (!this.props.stepsNavigation || evt.target.value === this.state.compState) {
         evt.preventDefault();
@@ -130,7 +131,7 @@ export default class StepZilla extends Component {
       // evt is a react event so we need to persist it as we deal with aync promises which nullifies these events (https://facebook.github.io/react/docs/events.html#event-pooling)
       evt.persist();
 
-      const movingBack = evt.target.value < this.state.compState; // are we trying to move back or front?
+      const movingBack = targetValue < this.state.compState; // are we trying to move back or front?
       let passThroughStepsNotValid = false; // if we are jumping forward, only allow that if inbetween steps are all validated. This flag informs the logic...
       let proceed = false; // flag on if we should move on
 
@@ -150,7 +151,7 @@ export default class StepZilla extends Component {
               // ... 'some' that to get a decision on if we should allow moving forward
               passThroughStepsNotValid = this.props.steps
                 .reduce((a, c, i) => {
-                  if (i >= this.state.compState && i < evt.target.value) {
+                  if (i >= this.state.compState && i < targetValue) {
                     a.push(c.validated);
                   }
                   return a;
@@ -170,11 +171,11 @@ export default class StepZilla extends Component {
         .then(() => {
           // this is like finally(), executes if error no no error
           if (proceed && !passThroughStepsNotValid) {
-            if (evt.target.value === (this.props.steps.length - 1) &&
+            if (targetValue === (this.props.steps.length - 1) &&
               this.state.compState === (this.props.steps.length - 1)) {
               this.setNavState(this.props.steps.length);
             } else {
-              this.setNavState(evt.target.value);
+              this.setNavState(targetValue);
             }
           }
         })
